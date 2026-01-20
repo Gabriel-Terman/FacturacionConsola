3d77254d2bec3005b83cb2ebc75ee1f69e2725ab
Gabriel Terman <rambelterman@gmail.com>
2026-01-20 15:42:22 -0400

    Update Invoice.cs

--- FacturacionConsola/FacturacionConsola/Domain/Invoice.cs
+++ FacturacionConsola/FacturacionConsola/Domain/Invoice.cs
@@ -6,15 +6,16 @@ namespace FacturacionConsola.Domain
 {
     public class Invoice
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString("N");
-        public string Number { get; set; } = ""; // podría ser secuencial simple
+        public int Id { get; set; } = 0; // lo asigna el repositorio 
+        public string Number { get; set; } = ""; // Asigna un valor inicial vacío a la propiedad cuando se crea una instancia de la clase (número correlativo simple)
         public DateTime Date { get; set; } = DateTime.Now;
         public List<InvoiceItem> Items { get; set; } = new();
-
+    
         public decimal Subtotal() => Items.Sum(i => i.LineSubtotal());
-
+    
         public decimal Tax(Services.ITaxCalculator calculator) => calculator.CalculateTax(Items);
-
+    
         public decimal Total(Services.ITaxCalculator calculator) => Subtotal() + Tax(calculator);
     }
 }
+
